edfa3460b68cbc896b09e885c7149e8408e46a88
KaueMagnus <kmarquesmagnus@gmail.com>
2025-07-11 11:16:14 -0300

    Tipo Boolean

---
+++ fundamentosJS/type-boolean.js
@@ -0,0 +1,12 @@
+// Tipo Boolean - TRUE e FALSE
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+let cadastroAtivado = false;
+
+//console.log(primeiroNumero === segundoNumero); 
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2);
