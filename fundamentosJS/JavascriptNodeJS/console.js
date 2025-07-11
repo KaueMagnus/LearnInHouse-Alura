8c8a3677aaa24a52dc3f2ec5475f9ead95b42edf
KaueMagnus <kmarquesmagnus@gmail.com>
2025-07-11 15:55:29 -0300

    Utilizando métodos do Console (log, error, table, time e trace)

---
+++ fundamentosJS/JavascriptNodeJS/console.js
@@ -0,0 +1,21 @@
+
+// log -> registro
+
+const variavel = true;
+
+console.log(245);
+console.log("Meu nome é Kaue");
+console.log(variavel);
+
+// Tratamento de erros
+
+console.error(ReferenceError, "Algo deu errado!")
+
+
+// Outros métodos importantes
+/*
+console.error() para exibir mensagens de erro;
+console.table() para visualizar de forma mais organizada informações tabulares;
+console.time() e console.timeEnd() para temporizar período que uma operação de código leva para ser iniciada e concluída;
+console.trace() para exibir a stacktrace de todos os pontos (ou seja, os arquivos chamados) por onde o código executado passou durante a execução.
+*/
