ed82c24c567c90ebfac3c9a91275c63e54c5d53f
KaueMagnus <kmarquesmagnus@gmail.com>
2025-07-11 11:05:27 -0300

    Tipo String

---
+++ fundamentosJS/type-string.js
@@ -0,0 +1,18 @@
+// Tipo String
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura321";
+const stringDeNumeros = "456123";
+
+const citacao = "Meu nome é ";
+const meuNome = "Kaue";
+
+// console.log(citacao)
+
+// concatenação (+)
+console.log(citacao + meuNome);
+
+// método .length()
+console.log(meuNome.length)
+
+
