b947fc36beaa7240252144345b4e98e49c023dac
KaueMagnus <kmarquesmagnus@gmail.com>
2025-07-11 14:32:42 -0300

    Conversões de tipos. (implícita e explícita)

---
+++ fundamentosJS/DeclarandoVariaveis/conversoes.js
@@ -0,0 +1,12 @@
+// Tipo de dado
+// Booleanos
+
+// Conversão implícita
+const numero = 123;
+const numeroString = "123";
+
+console.log(numero + numeroString);
+
+// Conversão explícita   Number() e String()
+
+console.log(numero + Number(numeroString));
