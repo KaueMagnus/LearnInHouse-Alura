93e4f3ef2f99fa0823227fe1c62858f6b13bac4e
KaueMagnus <kmarquesmagnus@gmail.com>
2025-07-11 16:06:53 -0300

    Template literal

---
+++ fundamentosJS/Operadores/template-strings.js
@@ -0,0 +1,9 @@
+const nome = "Kaue";
+const idade = 2025-2004;
+const cidadeDeNascimento = "Sombrio";
+
+// const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao)
